2adcdac59cb42cfcdd2ff420627001d67c5fc240
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2024-03-15 12:49:21 +0100

    New REST scipts

--- app.js
+++ app.js
@@ -403,6 +403,54 @@ var upp = schedule.scheduleJob(rule, function () {
 /*
 * REST jobs
 */
+var restValidateRRRecs = schedule.scheduleJob(rule, function () {
+	console.log('About to run REST Validate RR records');
+	var request = require('request');
+	request(rest_url + 'batchJobs/validateRecords.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('Validate RR records Run');
+			console.log(body); // Print whatever came back.
+		} else {
+			console.log('Error running Validate RR records');
+			// console.log(error);
+			// console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var restCorrectRRRecs = schedule.scheduleJob(rule, function () {
+	console.log('About to run REST Correct RR records');
+	var request = require('request');
+	request(rest_url + 'batchJobs/correctRRRecords.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('Correct RR records Run');
+			console.log(body); // Print whatever came back.
+		} else {
+			console.log('Error running Correct RR records');
+			// console.log(error);
+			// console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var restAddMissingHours = schedule.scheduleJob(rule, function () {
+	console.log('About to run REST Add Missing Hours');
+	var request = require('request');
+	request(rest_url + 'batchJobs/addMissingHours.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('Add Missing Hours Run');
+			console.log(body); // Print whatever came back.
+		} else {
+			console.log('Error running Add Missing Hours');
+			// console.log(error);
+			// console.log(response);
+			console.log(body);
+		}
+	});
+});
+
 var restVBACActivePersons = schedule.scheduleJob({ hour: 1, minute: 45 }, function () {
 	console.log('About to run REST loadVBACActivePersons');
 	var request = require('request');
